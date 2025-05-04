b4dd9ce9b9cba13be4fc773787eb4db39f582f93
IveyLeagueTech <33326457+IveyLeagueTech@users.noreply.github.com>
2025-05-04 19:54:14 -0400

    Add files via upload

---
+++ scripts/script2.js
@@ -0,0 +1,73 @@
+/* 
+Student Name: Ivey Rothenberg
+File Name: script.js
+Date: 04/18/2025
+*/
+
+// Hamburger menu function
+function menu() {
+    var navlinks = document.getElementById("nav-links");
+    var menuicon = document.getElementById("icon");
+
+        // Toggle visibility and aria-expanded
+        if (navlinks.style.display === "block") {
+            navlinks.style.display = "none";
+            menuicon.style.color = "#2a1f14";
+            menuicon.setAttribute("aria-expanded", "false");
+        } else {
+            navlinks.style.display = "block";
+            menuicon.style.color = "#f6eee4";
+            menuicon.setAttribute("aria-expanded", "true");
+        }
+    }
+
+// Global variables
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
+
+// FAQ answer functions
+function ans1() {
+    answer.textContent = "If you don’t see a parent after several hours and the animal is cold or weak, it may be orphaned.";
+}
+
+function ans2() {
+    answer.textContent = "Signs of rabies can include excessive drooling, aggression, or acting unusually tame. Do not approach the animal.";
+}
+
+function ans3() {
+    answer.textContent = "No. Most birds have a poor sense of smell and will not abandon babies if touched by humans.";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+    // Get the back-to-top button
+    const backToTopButton = document.getElementById("hammer-btn");
+  
+      
+    // Smooth scroll back to top when the button is clicked
+    backToTopButton.addEventListener("click", function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", 
+      });
+    });
+  });
+
+
+// mobile hammer script to show on scroll
+
+const hammerBtn = document.getElementById('hammer-btn');
+
+window.addEventListener('scroll', () => {
+  if (window.innerWidth <= 768) {
+    if (window.scrollY > 200) {
+      hammerBtn.classList.add('visible');
+    } else {
+      hammerBtn.classList.remove('visible');
+    }
+  }
+});
+
+hammerBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
